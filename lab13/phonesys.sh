16166ba725d49454f8d1597c6c773df7ce14c586
andy <ashiraki@anduril.com>
2021-05-15 22:29:05 -0700

    work on add

--- lab13/phonesys.sh
+++ lab13/phonesys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SEL=0;
-while [ $SEL -ne 5 ]; 
+while [[ $SEL != "5" ]]; 
 do
 SEL=0;
 clear	
@@ -11,12 +11,27 @@ case $SEL in
 	"5")
 		exit 0;;
 	"1")
-		./add.sh
-		if [ $? = 0 ]; then
-			echo -e "\t\tAdded Successfully!"
-		elif [ $? = 1 ]; then
-			echo -e "\t\tEntry already exists!"
-		fi
+		MOAR=1
+		YN='0'
+		while [ $MOAR -ne 0 ]
+		do
+			./add.sh
+			if [ $? = 0 ]; then
+				echo -e "\t\tAdded Successfully!"
+			elif [ $? = 1 ]; then
+				echo -e "\t\tEntry already exists!"
+			fi
+			echo -e "\t\tWould you like to add another? (y/n) \c"
+			read YN;
+			while [[ $YN != 'n' ]] && [[ $YN != 'y' ]] 
+			do	
+				echo -e "\t\tWould you like to add another? (y/n) \c"
+				read YN;
+			done
+			if [ $YN = 'n' ]; then
+				MOAR=0;
+			fi	
+		done
 		;;
 	"2")
 		#del
